bf02ec398bf9f5548aada2846f31fdefc2670474
Aden-Liu <124870831+Aden-Liu@users.noreply.github.com>
2023-08-08 17:31:54 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const nameError = document.getElementById("name-error");
+const phoneError = document.getElementById("phone-error");
+const emailError = document.getElementById("email-error");
+const messageError = document.getElementById("message-error");
+const submitError = document.getElementById("submit-error");
+
+function validateName() {
+  let name = document.getElementById("contact-name").value;
+
+  if (name.length == 0) {
+    nameError.innerHTML = "Enter Full Name";
+  } else if (!name.match(/^[A-Za-z])) {
+
+  }
+}
